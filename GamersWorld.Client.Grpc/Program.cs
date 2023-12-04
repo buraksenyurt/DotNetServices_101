30a6935de25f96af6136bb60ed7f824d76ba66f3
buraksenyurt <burakselimsenyurt@gmail.com>
2023-12-04 21:17:23 +0300

    Added gRPC client app

---
+++ GamersWorld.Client.Grpc/Program.cs
@@ -0,0 +1,23 @@
+﻿using GamersWorld.Client.Grpc;
+using Grpc.Net.Client;
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5034");
+var service = new GameBox.GameBoxClient(channel);
+
+var addedGame = service.AddGame(new AddGameRequest
+{
+    Title = "1942",
+    Point = 8.5,
+    OnSale = true,
+    ReleaseDate = 1984
+});
+
+Console.WriteLine($"{addedGame.GameId} - {addedGame.Title}");
+
+Console.WriteLine("Tüm oyunlar");
+var games = service.GetGames(new GetGamesRequest()).Data;
+
+foreach (var game in games)
+{
+    Console.WriteLine($"{game.GameId} - {game.Title}");
+}
